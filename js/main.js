b9d693512eb0f585de8bff7414e5cbbe9c795b5c
meed ghoufrane <baloncesmeed@gmail.com>
2024-02-05 09:41:35 +0100

    edit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const prevBtn = document.getElementById('prevBtn');
     const nextBtn = document.getElementById('nextBtn');
     let page = 1;
-    const itemsPerPage = 10; // Adjust as needed
+    const itemsPerPage = 10; 
     let totalPages = 0;
   
     const fetchRepos = async (page, perPage) => {
@@ -20,17 +20,17 @@ document.addEventListener('DOMContentLoaded', async () => {
   
     const renderRepo = (repo) => {
         const li = document.createElement('li');
-        li.classList.add('repo-item'); // Add a class to the list item for styling
+        li.classList.add('repo-item'); 
       
         const repoContainer = document.createElement('div');
-        repoContainer.classList.add('repo-container'); // Add a class to the container div for styling
+        repoContainer.classList.add('repo-container'); 
       
         const imageDiv = document.createElement('div');
-        imageDiv.classList.add('image-container'); // Add a class to the image container div for styling
+        imageDiv.classList.add('image-container'); 
         imageDiv.innerHTML = `<img src="${repo.owner.avatar_url}" alt="${repo.owner.login}'s avatar">`;
       
         const detailsDiv = document.createElement('div');
-        detailsDiv.classList.add('details-container'); // Add a class to the details container div for styling
+        detailsDiv.classList.add('details-container'); 
         detailsDiv.innerHTML = `
           <h3>${repo.name}</h3>
           <p>${repo.description}</p>
@@ -66,22 +66,22 @@ document.addEventListener('DOMContentLoaded', async () => {
     };
   
     const renderRepos = async () => {
-      repoList.innerHTML = ''; // Clear existing repositories
+      repoList.innerHTML = ''; 
       const repos = await fetchRepos(page, itemsPerPage);
       repos.forEach(renderRepo);
       renderPagination();
     };
   
-    // Fetch total count for pagination
+ 
     const totalCountResponse = await fetch(`https://api.github.com/search/repositories?q=stars:>1`);
     const totalCountData = await totalCountResponse.json();
     const totalCount = totalCountData.total_count;
     totalPages = Math.ceil(totalCount / itemsPerPage);
   
-    // Initial fetch and render
+    
     await renderRepos();
   
-    // Event listeners for pagination buttons
+   
     nextBtn.addEventListener('click', nextPage);
     prevBtn.addEventListener('click', prevPage);
   });
